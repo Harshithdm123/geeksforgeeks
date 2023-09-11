0a8e073081e11eb9bdd2f695afc7fa6257e02dba
Harshith D M <118685275+Harshithdm123@users.noreply.github.com>
2023-09-11 21:44:01 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Binary Search/binary-search.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+  public class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int key = sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.binarysearch(arr, n, key));
+            T--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        
+   int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == k) {
+                return mid; // Found the target element; return its index.
+            } else if (arr[mid] < k) {
+                left = mid + 1; // Target is in the right half of the array.
+            } else {
+                right = mid - 1; // Target is in the left half of the array.
+            }
+        }
+
+        return -1;
+        // code here
+    }
+}
