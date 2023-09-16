eccb0497674e151e1e972dbdd49f38677e7574fa
Harshith D M <118685275+Harshithdm123@users.noreply.github.com>
2023-09-16 21:58:44 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum of all subarrays of size k/maximum-of-all-subarrays-of-size-k.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        //taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total count of testcases
+        int t = sc.nextInt();
+        
+        
+        
+        while(t-- > 0)
+        {
+            //taking total number of elements
+            int n = sc.nextInt();
+            
+            //taking size of subArrays 
+            int k = sc.nextInt();
+            
+            //Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+            
+            //adding all the elements to the array 
+            for(int i = 0; i <n; i++)
+            {
+                arr[i] =sc.nextInt();
+            }
+            
+            //Calling the method max_of_subarrays of class solve
+            //and storing the result in an ArrayList
+            ArrayList <Integer> res = new Solution().max_of_subarrays(arr, n, k);
+            
+            //printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find maximum of each subarray of size k.
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+         ArrayList<Integer> arr1 = new ArrayList<>();
+        int start=0;
+        int count=k;
+        int j=0;
+        int max=0;
+       while(start!=n-k+1){
+           if(count>0){
+                max= Math.max(max,arr[j]);
+                j++;
+                count--;
+           }
+           if(count==0){
+               arr1.add(max);
+               start++;
+               count=k;
+               j=start;
+               max=0;
+           }
+       }
+        return arr1;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Your code here
+    }
+}
