24383b2897a3335b6950d8689177e5a1e1f1a858
Harshith D M <118685275+Harshithdm123@users.noreply.github.com>
2023-11-20 22:28:05 +0530

    Updated Solution - GfG to GitHub

--- Basic/Searching a number/searching-a-number.java
+++ Basic/Searching a number/searching-a-number.java
@@ -35,6 +35,8 @@ class Main {
 class Solution {
     public int search(int arr[], int n, int k) {
 
+
+
          for(int i=0;i<n;i++)    
             if(arr[i] == k)    
                 return i+1;    
