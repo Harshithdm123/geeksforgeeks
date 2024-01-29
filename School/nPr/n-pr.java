72bf70947e22e900a52efbf1a0fdd370919b3e7f
Harshith D M <118685275+Harshithdm123@users.noreply.github.com>
2024-01-29 20:43:55 +0530

    Added Solution - GfG to GitHub

---
+++ School/nPr/n-pr.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String a[] = in.readLine().trim().split("\\s+");
+            Long n = Long.parseLong(a[0]);
+            Long r = Long.parseLong(a[1]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.nPr(n, r));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static long nPr(long n, long r){
+        return factorial(n)/factorial(n-r);
+       
+    }
+    static long factorial(long f){
+        long fact=1;
+        for(long i=1;i<=f;i++){
+            fact=fact*i;
+        }
+        return fact;
+       
+    }
+}
