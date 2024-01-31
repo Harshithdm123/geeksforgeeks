9c77ccfb651575f4cfe2de2e5beac560e98f37c2
Harshith D M <118685275+Harshithdm123@users.noreply.github.com>
2024-01-31 22:35:07 +0530

    Updated Solution - GfG to GitHub

--- Easy/Missing number in array/missing-number-in-array.java
+++ Easy/Missing number in array/missing-number-in-array.java
@@ -29,15 +29,14 @@ class GFG {
 
 class Solution {
     int missingNumber(int array[], int n) {
-      
-      int sum=0;
-      for(int i=0;i<n-1;i++){
-          sum=sum+array[i];
-          
-          
-      }
-       return (n*(n+1)/2)-sum;
-    
-      
+        
+        
+        Arrays.sort(array);
+        for(int i=0;i<n-1;i++){
+            if(array[i]!=i+1){
+                return i+1;
+            }
+        }
+        return n;
     }
 }
