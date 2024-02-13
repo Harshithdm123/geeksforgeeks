985da7a6821e8e1a887f4f20a1e60780ba24f65a
Harshith D M <118685275+Harshithdm123@users.noreply.github.com>
2024-02-13 18:54:44 +0530

    Updated Solution - GfG to GitHub

--- School/Sum of Series/sum-of-series.java
+++ School/Sum of Series/sum-of-series.java
@@ -1,21 +1,22 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
 import java.io.*;
 import java.util.*;
 
 class GFG {
-    // Driver code
-    public static void main(String[] args) throws Exception {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-
-            long ans = new Solution().seriesSum(n);
-
-            System.out.println(ans);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            long res = obj.seriesSum(n);
+            
+            System.out.println(res);
+            
         }
     }
 }
@@ -23,18 +24,15 @@ class GFG {
 // } Driver Code Ends
 
 
-// User function Template for Java
 
 class Solution {
-    // function to return sum of  1, 2, ... n
-    long seriesSum(int n) {
-        
-    
-        
-      return (long)n*(n+1)/2;
-        
-        
-        
-        // code here
+    public static long seriesSum(int n) {
+           long sum = 0;
+        for(int i=1; i<=n; i++){
+            sum= (long) n*(n+1)/2;
+            
+        }
+        return sum;
     }
 }
+        
