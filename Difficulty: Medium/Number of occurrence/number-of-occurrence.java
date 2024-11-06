77c8fdd95491bddb16917d672ec5cb309143f792
Harshith D M <118685275+Harshithdm123@users.noreply.github.com>
2024-11-06 17:58:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Medium/Number of occurrence/number-of-occurrence.java
@@ -1,90 +1,51 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
-
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) throws Exception {
+class GFG {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine().trim());
-        while (tc-- > 0) {
-            String[] inputLine;
-            inputLine = br.readLine().trim().split(" ");
-            int n = Integer.parseInt(inputLine[0]);
-            int x = Integer.parseInt(inputLine[1]);
-            int[] arr = new int[n];
-            inputLine = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
 
-            int ans = new Solution().count(arr, n, x);
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.count(a, k);
+
             System.out.println(ans);
+            System.out.println("~");
         }
     }
 }
-
 // } Driver Code Ends
 
 
-//User function Template for Java
-
-
-
 class Solution {
-    int count(int[] arr, int n, int x) {
-      
-  int first = firstOccurrence(arr, n, x);
+    int count(int[] arr, int target) {
+        // code here
         
-        if (first == -1) {
-            return 0;
-        }
+       int n=arr.length;
+       int count =0;
         
-        int last = lastOccurrence(arr, n, x);
-        
-        return last - first + 1;
-    }
-
-   
-    int firstOccurrence(int[] arr, int n, int x) {
-        int low = 0, high = n - 1, result = -1;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            if (arr[mid] == x) {
-                result = mid;
-                high = mid - 1; 
-            } else if (arr[mid] < x) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        for(int i=0;i<n;i++){
+            if(arr[i]==target){
+                count++;
+                
             }
-        }
-        
-        return result;
-    }
-
-    int lastOccurrence(int[] arr, int n, int x) {
-        int low = 0, high = n - 1, result = -1;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
             
-            if (arr[mid] == x) {
-                result = mid;
-                low = mid + 1; 
-            } else if (arr[mid] < x) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            
+            
         }
+        return count;
         
-        return result;
     }
 }
