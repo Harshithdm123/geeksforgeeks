2e72d8fb01c69ad987f86b3883c046b6b4a4b45e
Harshith D M <118685275+Harshithdm123@users.noreply.github.com>
2024-11-17 17:37:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Run Length Encoding/run-length-encoding.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            String s;
+            s = br.readLine();
+            
+            Solution obj = new Solution();
+            String res = obj.encode(s);
+            
+            System.out.println(res);
+            
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static String encode(String s) {
+        // code here
+        StringBuilder sb=new StringBuilder();
+        
+        for(int i=0;i<s.length();i++){
+            int count=1;
+            
+            char ch=s.charAt(i);
+            
+            while(i<s.length()-1&&s.charAt(i)==s.charAt(i+1)){
+                count++;
+                i++;
+                
+                
+                
+                
+            }sb.append(ch);
+            sb.append(count);
+            
+            
+            
+            
+        }
+        return sb.toString();
+        
+        
+        
+    }
+}
+    
